ee4a308aed36156ce52a92e9d3e7c406bb02d44b
JaviDelSolar <133437289+JaviDelSolar@users.noreply.github.com>
2023-08-07 16:55:30 -0400

    Add files via upload

---
+++ Día 6/Onclick/js/index.js
@@ -0,0 +1,13 @@
+console.log("correcto"); 
+
+function mensaje(elemento){
+    elemento.innerHTML="Logout";
+}
+/*Listo!*/
+function alertaclick(){
+    alert("Ninja was liked");
+}
+
+function ocultarBoton(elemento) {
+    elemento.remove();
+}
